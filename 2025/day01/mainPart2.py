b063c1afc64ac964b82eb867a8e99e3210f94dc4
Nivre0 <timleu256@gmail.com>
2025-12-08 07:37:55 +0100

    2025 day 01 p1

--- 2025/day01/mainPart2.py
+++ 2025/day01/mainPart2.py
@@ -1,5 +1,26 @@
 file = "input.txt"
 print("using input:", file)
 
-with open(file, 'r') as file:
-    lines = file.read().splitlines()
+with open(file, 'r') as f:
+    lines = f.read().splitlines()
+    position = 50
+    count = 0
+
+    for line in lines:
+        direction = line[0]
+        number = int(line[1:])
+
+        if direction == "R":
+            hit_i = (100 - position) % 100
+        else:
+            hit_i = position % 100
+
+        if 1 <= hit_i <= number:
+            count += 1
+
+        if direction == "R":
+            position = (position + number) % 100
+        else:
+            position = (position - number) % 100
+
+    print(count)
